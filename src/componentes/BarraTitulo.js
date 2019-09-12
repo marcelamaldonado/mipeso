ebbf876bef767b55ce075a730a086c7ef4d2960b
Marcela <marcela.desarrollosweb@gmail.com>
2019-09-11 22:20:27 -0300

    se instalo materialize y highcharts

---
+++ src/componentes/BarraTitulo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export default()=>(
+    <div>
+    <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Mi Registro de Peso</a>
+      
+    </div>
+  </nav>
+    </div>
+)
