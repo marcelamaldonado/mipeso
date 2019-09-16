81b511b2170b2afae78f10ebc22474bcd435fd30
Marcela <marcela.desarrollosweb@gmail.com>
2019-09-16 11:29:53 -0300

    cambios en el formulario

--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -1,28 +1,61 @@
+import 'react-datepicker/dist/react-datepicker.min.css'
 import React from 'react'
 import './Form.css'
+import swal from 'sweetalert'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
+
 
 export default class Form extends React.Component{
-    onSubmit = (evt)=>{
-        evt.preventDefault()
-        const fecha = evt.target.fecha.value
-        const peso = evt.target.peso.value
-        console.log(fecha,peso)
-    }
+    
+    //state = {
+    //    fecha:moment(),
+     //   peso:null
+    //};
+    state = {
+        fecha: new Date(),
+        peso: null
+      };
+    onSubmit = ()=>{
+        const {fecha,peso} = this.state;
+
+        console.log(fecha,peso);
+        if (!peso || peso < 0){
+            swal('Lectura invalida', 'El registro de peso debe ser valido','error')
+        }
+    };
+    cambioFecha = fecha => {
+        this.setState({fecha});
+    };
+    cambioPeso = evt =>{
+    this.setState({
+        peso:evt.target.value
+    })
+    };
     render(){
         return(
             <div className="row">
                 <div className="form-container col s4 offset-s4 z-depth-4 cyan lighten-3">
-                    <form onSubmit={this.onSubmit}>
+                    <form>
                         <label htmlFor="fecha">
-                        fecha:
-                        <input type="date" name="fecha" id="fecha"/>
+                        Fecha:
+                        <DatePicker
+                            locale = "es"
+                            selected={this.state.fecha}
+                            onChange={this.cambioFecha}
+                        /> {/*<input type="date" name="fecha" id="fecha"/>*/}
                         </label>
                     <label htmlFor="peso">
                         Peso:
-                        <input Type="text" name="peso" id="peso"/>
-                    </label>
-                        <label/>
-                        <input type="submit" value="Agregar"/>
+                        <input Type="text" 
+                        name="peso" value={this.state.peso}
+                        onChange={this.cambiarPeso} id="peso"/>
+                      </label>
+                        <input type="button" onClick={this.onSubmit} value="Agregar"/>
+                        <input type="button" value="Cerrar"/>
                     </form>
                 </div>
             </div>
