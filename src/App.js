3f0b5cdcd9017ce84d895b637f83964ce4cdc3ad
Marcela <marcela.desarrollosweb@gmail.com>
2019-09-18 09:34:44 -0300

    funciona el boton de cerrar

--- src/App.js
+++ src/App.js
@@ -9,48 +9,66 @@ import Form from "./componentes/Form"
 // [1, 2, 3, 4, 5, 6, 7].map(dia =>
 // [+moment().add(dia, 'days'), Math.random() * 200])
 moment.locale('es')
+
 class App extends Component {
   state = {
-    registros: []
+    registros: [],
+    modal: false
 
+  };
+  //onCrearRegistro = ()=>{
+  //  const nuevoregistro = [+moment(),Math.random() * 200] 
+  // this.setState({
+  //  registros: [...this.state.registros,nuevoregistro]
+  //})
+  //}
+
+  aceptarRegistro = ({ fecha, peso }) => {
+    console.log(fecha, peso)
+    const nuevoregistro = [+fecha, +peso]
+    console.log(nuevoregistro)
+    this.setState((prevState, props) => ({
+      registros: [...prevState.registros, nuevoregistro]
+    }));
+  };
+  onCerrarForm = ()=>{
+    this.setState({
+      modal:false
+    })
   }
-  
-  onCrearRegistro = ()=>{
-     const nuevoregistro = [+moment(),Math.random() * 200] 
-     this.setState({
-       registros: [...this.state.registros,nuevoregistro]
-     })
-  }
-  
+
   render() {
     const btnAdd = {
       position: "absolute",
-      top:"80%",
-      right:"10%"
+      top: "80%",
+      right: "10%"
     }
-    
+
     return (
       <div>
-      <Form/>
-    <BarraTitulo />
+        <Form
+          visible={this.state.modal} onAceptar={this.aceptarRegistro} onCerrar={this.onCerrarForm}
+        />
+        <BarraTitulo />
         <main>
           <div className="valign-wrapper">
             <h3>Registro Diario de Peso</h3>
           </div>
           <div className="row">
             <div className="col l6 m12 s12">
-              <Grafica registros = {this.state.registros}/>
+              <Grafica registros={this.state.registros} />
             </div>
             <div className="col l6 m12 s12">
-              <Tabla registros = {this.state.registros}/>
+              <Tabla registros={this.state.registros} />
             </div>
           </div>
           <a className="btn-floating btn-large waves-effect waves-light red"
-          onClick={this.onCrearRegistro}
-          style={btnAdd}
-          ><i className="material-icons">add</i></a>
+            style={btnAdd}
+            onClick={() => this.setState({ modal: true })}
+          >
+            <i className="material-icons">add</i></a>
         </main>
-        
+
       </div>
     );
   }
--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -4,9 +4,7 @@ import './Form.css'
 import swal from 'sweetalert'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
-import es from 'date-fns/locale/es';
-registerLocale('es', es)
+import { dateFormat } from 'highcharts'
 
 
 export default class Form extends React.Component{
@@ -17,46 +15,47 @@ export default class Form extends React.Component{
     //};
     state = {
         fecha: new Date(),
-        peso: null
+        peso: ""
       };
     onSubmit = ()=>{
         const {fecha,peso} = this.state;
 
-        console.log(fecha,peso);
         if (!peso || peso < 0){
             swal('Lectura invalida', 'El registro de peso debe ser valido','error')
+        }else{
+            this.props.onAceptar(this.state)
         }
     };
     cambioFecha = fecha => {
         this.setState({fecha});
     };
-    cambioPeso = evt =>{
+    cambiarPeso = evt =>{
     this.setState({
         peso:evt.target.value
     })
     };
-    render(){
-        return(
-            <div className="row">
-                <div className="form-container col s4 offset-s4 z-depth-4 cyan lighten-3">
-                    <form>
-                        <label htmlFor="fecha">
-                        Fecha:
-                        <DatePicker
-                            locale = "es"
-                            selected={this.state.fecha}
-                            onChange={this.cambioFecha}
-                        /> {/*<input type="date" name="fecha" id="fecha"/>*/}
-                        </label>
-                    <label htmlFor="peso">
-                        Peso:
-                        <input Type="text" 
-                        name="peso" value={this.state.peso}
-                        onChange={this.cambiarPeso} id="peso"/>
-                      </label>
-                        <input type="button" onClick={this.onSubmit} value="Agregar"/>
-                        <input type="button" value="Cerrar"/>
-                    </form>
+render(){
+    return(
+       <div className="row">
+        <div className={`form-container scale-transition scale-out ${this.props.visible ? "scale-in":""} col s4 offset-s4 z-depth-4 cyan lighten-3`}>
+        <form>
+        <label htmlFor="fecha">
+         Fecha:
+          <DatePicker
+          locale = "es"
+         selected={this.state.fecha}
+          onChange={this.cambioFecha}
+        /> {/*<input type="date" name="fecha" id="fecha"/>*/}
+             </label>
+            <label htmlFor="peso">
+             Peso:
+             <input Type="text" 
+             name="peso" value={this.state.peso}
+             onChange={this.cambiarPeso} id="peso"/>
+             </label>
+             <input type="button" className="btn" onClick={this.onSubmit} value="Agregar"/>
+             <input type="button" className="btn" onClick={()=>this.props.onCerrar()} value="Cerrar"/>
+             </form>
                 </div>
             </div>
         );
